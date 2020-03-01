6569c5de0f67a643ba78dc5facb9a157ad4ed12b
Aleksander Rezen <alexrezen@bric-tps.com>
2020-02-29 20:25:27 -0800

    done

---
+++ vars/grailsApplicationPipelineTest.groovy
@@ -0,0 +1,53 @@
+#!/usr/bin/groovy
+
+def call(body) {
+    def params = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = params
+    body()
+
+    pipeline {
+        agent any
+        stages {
+            stage('Initialize') {
+                steps {
+                    echoEnvironment()
+                }
+            }
+            stage('Build') {
+                steps {
+                    echo "${NEXUS_REPO}"
+                    sh './gradlew clean build -x test'
+                }
+            }
+            stage('Test') {
+                steps {
+                    sh './gradlew test -x cobertura'
+                }
+            }
+            stage('Code Coverage') {
+                steps {
+                    sh './gradlew cobertura'
+                    cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/build/reports/cobertura/coverage.xml', conditionalCoverageTargets: '70, 0, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false
+                }
+            }
+            stage('Publish') {
+                steps {
+                    sh './gradlew prepareBuild'
+                    script {
+                        docker.withRegistry($nexusURL, 'repoPusher-credentials') {
+                            def customImage = docker.build("jenkins-test",  "./build/docker")
+                            // tag step
+                            customImage.push()
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
